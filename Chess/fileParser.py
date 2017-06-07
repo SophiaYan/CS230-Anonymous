19c76ea8e3c2cfc3854038afa372b89e483a6669
Jingyu Shao <shaojy11@gmail.com>
2017-06-06 17:37:07 -0700

    Recursively build json file by DFS file directory traversal

--- Chess/fileParser.py
+++ Chess/fileParser.py
@@ -1,33 +1,74 @@
 import numpy as np
-import os
+import os, sys
 import glob
 import csv
 import fnmatch
+from stat import *
+import json
+# allFiles = []
+# num = 1
+# for root, dirnames, filenames in os.walk(os.getcwd() + '/src'):
+# 	for fname in fnmatch.filter(filenames, '*.java'):
+# 		filename = os.path.join(root, fname)
+# 		# current file name
+# 		cur = filename.rsplit('/', 2)[1] + '/' + fname + ':'
 
-allFiles = []
-num = 1
-for root, dirnames, filenames in os.walk(os.getcwd() + '/src'):
-	for fname in fnmatch.filter(filenames, '*.java'):
-		filename = os.path.join(root, fname)
-		# current file name
-		cur = filename.rsplit('/', 2)[1] + '/' + fname + ':'
+# 		with open(filename) as f:
+# 			content = f.readlines()
+# 		lines = [x.strip('\n') for x in content]
+# 		for i,line in enumerate(lines):
+# 			index = line.find(' class ')
+# 			if (index != -1):
+# 				index = index + 7
+# 				line = line[index:]
+# 				# find all class names in the file
+# 				className = line.split(' ')[0]
+# 				cur = cur + ' ' + className
+# 				print num
+# 				num = num + 1
+# 				print cur
+# 				allFiles.append(str(cur))
 
-		with open(filename) as f:
-			content = f.readlines()
-		lines = [x.strip('\n') for x in content]
-		for i,line in enumerate(lines):
-			index = line.find(' class ')
-			if (index != -1):
-				index = index + 7
-				line = line[index:]
-				# find all class names in the file
-				className = line.split(' ')[0]
-				cur = cur + ' ' + className
-				print num
-				num = num + 1
-				print cur
-				allFiles.append(str(cur))
+# with open('ParseRes.csv','wb') as myfile:
+# 	wr = csv.writer(myfile, quoting = csv.QUOTE_ALL)
+# 	wr.writerow(allFiles)
 
-with open('ParseRes.csv','wb') as myfile:
-	wr = csv.writer(myfile, quoting = csv.QUOTE_ALL)
-	wr.writerow(allFiles)
+#os.chdir(os.getcwd() + "/src/controller")
+#for filename in glob.glob("*.java"):
+#    print filename
+    
+def walktree(top, callback, dict):
+    '''recursively descend the directory tree rooted at top,
+       calling the callback function for each regular file'''
+
+    for f in os.listdir(top):
+        pathname = os.path.join(top, f)
+        mode = os.stat(pathname)[ST_MODE]
+        if S_ISDIR(mode):
+            # It's a directory, recurse into it
+            callback(f)
+            subdict = {"name": f, "children" :[], "size": 0}
+            walktree(pathname, callback, subdict)
+            dict["children"].append(subdict)
+        elif S_ISREG(mode):
+            # It's a file, call the callback function (leaf node)
+            if f.endswith(".java"):
+                subdict = parse(top, f)
+                dict["children"].append(subdict)
+                callback(f)
+        else:
+            # Unknown file type, print a message
+            print 'Skipping %s' % pathname
+
+def parse(dir, filename):
+    leaf = {"name": filename, "size" : 0}
+    return leaf
+    
+def visitfile(file):
+    print 'visiting', file
+
+if __name__ == '__main__':
+    dict = {"name": "root", "children" :[], "size":0}
+    walktree(os.getcwd() + "/src", visitfile, dict)
+    with open('src.json', 'w') as outfile:
+        json.dump(dict, outfile, ensure_ascii=False, sort_keys = False)
--- Chess/mseParser.py
+++ Chess/mseParser.py
@@ -8,39 +8,43 @@ allFiles = []
 filename = os.getcwd() + '/src/src.mse'
 toMatch = 'FAMIX.Class'
 num = 1
-print filename
+
 with open(filename) as f:
-	content = f.readlines()
+    content = f.readlines()
 lines = [x.strip('\n') for x in content]
+
 for i, line in enumerate(lines):
-	index = line.find(toMatch, 2)
-	if (index != -1):
-		sourceAnchorLine = lines[lines.index(line) + 4]
-		classNameLine = lines[lines.index(line) + 1]
-		index1 = sourceAnchorLine.find('(sourceAnchor', 2)
-		index2 = classNameLine.find('(name ')
-		if (index1 == -1):
-			continue
-		if (index2 == -1):
-			continue
-
-		# find class name
-		className = classNameLine[(index2 + 7):]
-		className = className.split('\'')[0]
-
-		# find class file id
-		sourceAnchorId = sourceAnchorLine[(index1 + 20):]
-		sourceAnchorId = sourceAnchorId.split(')')[0]
-
-		# find file name
-		# TODO
-
-		cur = 'id: ' + sourceAnchorId + ' -- ' + className
-		print num
-		num = num + 1
-		print cur
-		allFiles.append(str(cur))
+    index = line.find(toMatch, 2)
+    if (index != -1):
+        sourceAnchorLine = lines[lines.index(line) + 4]
+        classNameLine = lines[lines.index(line) + 1]
+        index1 = sourceAnchorLine.find('(sourceAnchor', 2)
+        index2 = classNameLine.find('(name ')
+        if (index1 == -1):
+            continue
+        if (index2 == -1):
+            continue
+        
+        # find class name
+        className = classNameLine[(index2 + 7):]
+        className = className.split('\'')[0]
+        
+        # find class file id
+        sourceAnchorId = sourceAnchorLine[(index1 + 20):]
+        sourceAnchorId = sourceAnchorId.split(')')[0]
+        sourceAnchorString = '\t(FAMIX.FileAnchor (id: ' + sourceAnchorId + ')'
+        
+        # find file name
+        fileNameLine = lines[lines.index(sourceAnchorString) + 3]
+        fileName = fileNameLine[(fileNameLine.find('(fileName ', 2) + 11):]
+        fileName = fileName.split('\'')[0]
+        
+        cur = fileName + '(id: ' + sourceAnchorId + '): ' + className
+        print num
+        num = num + 1
+        print cur
+        allFiles.append(str(cur))
 
 with open('MseParseRes.csv','wb') as myfile:
-	wr = csv.writer(myfile, quoting = csv.QUOTE_ALL)
-	wr.writerow(allFiles)
+    wr = csv.writer(myfile, quoting = csv.QUOTE_ALL)
+    wr.writerow(allFiles)
