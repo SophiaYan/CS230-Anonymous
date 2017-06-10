c41ea4c05347c08502035518c5edc4d0985e7f9f
ytbao <ytbao@outlook.com>
2017-06-10 11:17:01 -0700

    Create dndtree.js

--- dependence/dndtree.js
+++ dependence/dndtree.js
@@ -1,29 +1,4 @@
-/*Copyright (c) 2013-2016, Rob Schmuecker
-All rights reserved.
 
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-
-* Redistributions of source code must retain the above copyright notice, this
-  list of conditions and the following disclaimer.
-
-* Redistributions in binary form must reproduce the above copyright notice,
-  this list of conditions and the following disclaimer in the documentation
-  and/or other materials provided with the distribution.
-
-* The name Rob Schmuecker may not be used to endorse or promote products
-  derived from this software without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL MICHAEL BOSTOCK BE LIABLE FOR ANY DIRECT,
-INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
-OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
-EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.*/
 
 
 // Get JSON data
@@ -388,15 +363,15 @@ treeJSON = d3.json("dependence.json", function(error, treeData) {
         // Compute the new tree layout.
         var nodes = tree.nodes(root).reverse(),
             links = tree.links(nodes);
-        var maxscore = 0;
+        //var maxscore = 0;
 
         // Set widths between levels based on maxLabelLength.
         nodes.forEach(function(d) {
             d.y = (d.depth * (maxLabelLength * 10)); //maxLabelLength * 10px
             // alternatively to keep a fixed scale one can set a fixed depth per level
             // Normalize for fixed-depth by commenting out below line
-            d.x = d.x; //BAO:x-1000
-            if(d.score > maxscore) maxscore = d.score;
+            //d.x = d.x; //BAO:x-1000
+            //if(d.score > maxscore) maxscore = d.score;
         });
 
         // Update the nodes…
@@ -421,7 +396,11 @@ treeJSON = d3.json("dependence.json", function(error, treeData) {
             .attr('class', 'nodeCircle')
             .attr("r", 0)
             .style("fill", function color(d) {
-                return "hsl(" + parseInt(240 / maxscore* d.score, 10) + ",90%,70%)";
+                if (d.score == 1) return "hsl(0, 70%, 50%)";
+                else if (d.score == 2) return "hsl(60, 70%, 50%)";
+                else if (d.score == 3) return "hsl(120, 70%, 50%)";
+                else if (d.score == 4) return "hsl(180, 70%, 50%)";
+                else return "hsl(240, 70%, 50%)";
             })
             .style("stroke", function(d) {return d._children ? "steelblue" : "#fff";})
             .style("fill-opacity", function(d) {return d.name == "root" ? 1e-6 : 1})
@@ -471,8 +450,13 @@ treeJSON = d3.json("dependence.json", function(error, treeData) {
         node.select("circle.nodeCircle")
             .attr("r", 4.5)
             .style("fill", function color(d) {
-                return "hsl(" + parseInt(240 / maxscore * d.score, 10) + ",90%,70%)";
-            })
+                if (d.score == 1) return "hsl(0, 70%, 50%)";
+                else if (d.score == 2) return "hsl(60, 70%, 50%)";
+                else if (d.score == 3) return "hsl(120, 70%, 50%)";
+                else if (d.score == 4) return "hsl(180, 70%, 50%)";
+                else return "hsl(240, 70%, 50%)";
+            
+             })
             .style("stroke", function(d) {return d._children ? "steelblue" : "#fff";})
             .style("fill-opacity", function(d) {return d.name == "root" ? 1e-6 : 1})
             .style("stroke-width", function(d) {return d.name == "root" ? "0" : "1.5px"});
